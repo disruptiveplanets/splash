3a92f46ee8ab9ff07977258cf59514f839deeb67
prajwal <prajwalniraula@gmail.com>
2020-09-08 17:36:43 -0400

    Newer setup tools required.

--- setup.py
+++ setup.py
@@ -13,6 +13,9 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/prajwal309/splash",
     packages=setuptools.find_packages(),
+    include_package_data = True,
+
+
     classifiers=[
         "Programming Language :: Python :: 3.6",
         "License :: OSI Approved :: MIT License",
@@ -20,6 +23,7 @@ setuptools.setup(
     ],
     python_requires='>=3.5',
     install_requires=[
+        'setuptools>=46.4.0'
         'astropy>3',  # astropy 3 doesn't install in Python 2, but is req for astroquery
         'matplotlib>=2.0.0',  # earlier has bug for "from astroquery.mast import Catalogs"
         'numpy',
@@ -31,6 +35,5 @@ setuptools.setup(
         'numdifftools',
         'lmfit',
         'transitleastsquares',
-        'lxml'
      ]
 )
